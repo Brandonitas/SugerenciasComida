9c9d29c1a9f49bedabae94d91356577ee17b8de1
Brandonitas <angiebrand10@hotmail.com>
2018-04-20 12:26:08 -0500

    App terminada

--- app/src/main/java/mx/itesm/edu/earthone/sugerenciascena/MainActivity.kt
+++ app/src/main/java/mx/itesm/edu/earthone/sugerenciascena/MainActivity.kt
@@ -2,13 +2,39 @@ package mx.itesm.edu.earthone.sugerenciascena
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    val cenas = arrayListOf("Hotdog", "Comida china","Quesadillas","Tamales")
+    val cal = arrayListOf("290", "1400", "300", "153")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        comidaText.text = cenas[0]
+        calorias.text = cal[0]
+        agrega.setOnClickListener{
+            val nuevacena = nuevaText.text.toString()
+            cenas.add(nuevacena)
+            nuevaText.text.clear()
+
+            val agregarcaloria = agregarCaloria.text.toString()
+            cal.add(agregarcaloria)
+            agregarCaloria.text.clear()
+
+        }
+
+        selecciona.setOnClickListener {
+            val aleatorio = Random()
+            val cenaAleatorio = aleatorio.nextInt(cenas.count())
+            comidaText.text = cenas[cenaAleatorio]
+            calorias.text = cal[cenaAleatorio]
+        }
 
     }
 }
